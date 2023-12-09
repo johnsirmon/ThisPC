4bab39fef97bdc000f0a385bcd0778ef92d55781
John Sirmon <johnsirmon@hotmail.com>
2023-12-09 07:49:57 -0500

    Update ThisPC.ps1

--- ThisPC.ps1
+++ ThisPC.ps1
@@ -1,17 +1,11 @@
 # Collect system information
 $OSInfo = Get-ComputerInfo -Property "OsName", "OsVersion"
 $Memory = Get-ComputerInfo -Property "CsTotalPhysicalMemory"
-$GraphicsCard = Get-WmiObject Win32_VideoController | Select-Object -First 1 Name, DriverVersion, AdapterRAM
+$GraphicsCards = Get-WmiObject Win32_VideoController | ForEach-Object { "$($_.Name) (Driver Version: $($_.DriverVersion), Memory: $([Math]::Round($_.AdapterRAM / 1GB, 2)) GB)" }
 $HardDrive = Get-WmiObject Win32_DiskDrive | Select-Object -First 1 Model, Size
 
 # Format the data into a paragraph
-$Paragraph = @"
-Operating System: $($OSInfo.OsName), Version: $($OSInfo.OsVersion)
-Total Physical Memory: $([Math]::Round($Memory.CsTotalPhysicalMemory / 1GB, 2)) GB
-Graphics Card: $($GraphicsCard.Name), Driver Version: $($GraphicsCard.DriverVersion), Memory: $([Math]::Round($GraphicsCard.AdapterRAM / 1GB, 2)) GB
-Hard Drive: $($HardDrive.Model), Size: $([Math]::Round($HardDrive.Size / 1GB, 2)) GB
-Additional System Information: [You can add more details here if needed]
-"@
+$Paragraph = "My laptop has the following specs. Operating System: $($OSInfo.OsName), Version: $($OSInfo.OsVersion). Physical Memory: $([Math]::Round($Memory.CsTotalPhysicalMemory / 1GB, 2)) GB. Graphics Card(s): $($GraphicsCards -join ', '). Hard Drive: $($HardDrive.Model), Size: $([Math]::Round($HardDrive.Size / 1GB, 2)) GB. Additional System Information: [You can add more details here if needed]"
 
 # Copy the paragraph to the clipboard
 $Paragraph | Set-Clipboard
